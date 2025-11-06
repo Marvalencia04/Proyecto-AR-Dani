0ec2bace1e1c249ea6727a2a430e86ae959f9be8
Adrian van Dixhoorn <adrian123van@gmail.com>
2025-11-06 23:04:35 +0100

    CanvasJuego

---
+++ Proyecto AR/Assets/Emilio/IrMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IrMenu : MonoBehaviour
+{
+    public void Irmenu()
+    {
+        // Cargar la escena especificada
+        SceneManager.LoadScene("MainMenu");
+    }
+}
